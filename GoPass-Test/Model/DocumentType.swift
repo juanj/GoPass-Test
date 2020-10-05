bb22d29cb7b33c7e91275288b3126c8e9c2a885d
Juan Jose <1958175+juanj@users.noreply.github.com>
2020-10-05 16:48:50 -0500

    Add documentType model

---
+++ GoPass-Test/Model/DocumentType.swift
@@ -0,0 +1,27 @@
+//
+//  DocumentType.swift
+//  GoPass-Test
+//
+//  Created by Juan on 5/10/20.
+//
+
+import Foundation
+
+struct DocumentType {
+    let id: String
+    let name: String
+
+    func abreviated() -> String {
+        return name.split(separator: " ")
+            .compactMap(\.first)
+            .map { $0.uppercased() }
+            .joined(separator: " ")
+    }
+}
+
+extension DocumentType: Codable {
+    enum CodingKeys : String, CodingKey {
+        case id
+        case name = "tipo"
+    }
+}
