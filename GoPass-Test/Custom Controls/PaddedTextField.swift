fe2bccd7c08c503111e0037ee9e073b5082cc09c
Juan Jose <1958175+juanj@users.noreply.github.com>
2020-10-05 16:36:06 -0500

    SignUpViewController Style

---
+++ GoPass-Test/Custom Controls/PaddedTextField.swift
@@ -0,0 +1,24 @@
+//
+//  PaddedTextField.swift
+//  GoPass-Test
+//
+//  Created by Juan on 5/10/20.
+//
+
+import UIKit
+
+class PaddedTextField: UITextField {
+    private var padding = UIEdgeInsets(top: 0, left: 5, bottom: 0, right: 5)
+
+    override open func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
+    }
+
+    override open func placeholderRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
+    }
+
+    override open func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
+    }
+}
--- GoPass-Test/Views/SignUpViewController.swift
+++ GoPass-Test/Views/SignUpViewController.swift
@@ -8,4 +8,8 @@
 import UIKit
 
 class SignUpViewController: UIViewController {
+
+    @IBAction func back(_ sender: Any) {
+        navigationController?.popViewController(animated: true)
+    }
 }
